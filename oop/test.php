e3aab90542b12b4d0b70535e58fc7dd96c0033a6
yousef <goeelkolaly@gmail.com>
2026-04-21 06:50:28 +0200

    new commit

--- oop/test.php
+++ oop/test.php
@@ -1,28 +1,31 @@
 <!-- <?php
 
-        // class Staff {
-        //     public $name;
+class Staf
+{
+    public $name;
 
-        //     public function work(){
-        //        return "Staff member is on duty  :" . $this->name;
-        //     }
+    public function work()
+    {
+        return "Staff member is on duty  :" . $this->name;
+    }
+}
 
-        // }
+class Trainer extends staf
+{
 
-        // class Trainer extends staff {
+    public function assignPlan()
+    {
+        return "Training plan has been created";
+    }
+}
+$train1 = new trainer();
+$train1->name = "ahmed";
 
-        //     public function assignPlan(){
-        //         return "Training plan has been created";
-        //     }
-        // } 
-        // $train1 = new trainer();
-        // $train1->name = "ahmed";
+echo $train1->assignPlan();
+echo "<br>";
+echo $train1->work();
 
-        // echo $train1->assignPlan();
-        // echo "<br>";
-        // echo $train1->work();
-
-        ?> -->
+?>
 
 
 
@@ -32,35 +35,40 @@
 
 <?php
 
-// class Staff {
-//     public function getSalary(){
-//         return "Base Salary : 1000";
-//     }
-// }
+class Staff
+{
+    public function getSalary()
+    {
+        return "Base Salary : 1000";
+    }
+}
 
-// class Manager extends Staff {
-//     public function getSalary(){
-//         return "manager salary :5000";
-//     }
-// }
+class Manager extends Staff
+{
+    public function getSalary()
+    {
+        return "manager salary :5000";
+    }
+}
 
-// class Coach extends Manager {
-//     public function getSalary()
-//     {
-//         return "coach salary :3000";
-//     }
-//     }
-//     $B1 = new staff();
-//     $B3 = new Coach();
-//     $B2 = new Manager();
+class Coach extends Manager
+{
+    public function getSalary()
+    {
+        return "coach salary :3000";
+    }
+}
+$B1 = new staff();
+$B3 = new Coach();
+$B2 = new Manager();
 
-//    echo $B1->getSalary()."<br>";
-//   echo $B2->getSalary()."<br>";
-//   echo  $B3->getSalary();
+echo $B1->getSalary() . "<br>";
+echo $B2->getSalary() . "<br>";
+echo  $B3->getSalary();
 
 
 ?>
-
+ -->
 
 
 <?php
@@ -69,22 +77,23 @@ abstract class Member
     abstract public  function getMembershipPrice();
 }
 
-class StudentMember extends Member {
+class StudentMember extends Member
+{
     public function getMembershipPrice()
     {
         return "1000";
     }
-
 }
 
 
-    class VipMember extends Member {
+class VipMember extends Member
+{
     public function getMembershipPrice()
     {
         return "500";
     }
-    }
+}
 
-    $bb = new VipMember();
-    echo $bb->getMembershipPrice();
+$bb = new VipMember();
+echo $bb->getMembershipPrice();
 ?>
