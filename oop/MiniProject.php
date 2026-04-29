b9346886d24969ff8f583172c637676d7e87ef41
yousef <goeelkolaly@gmail.com>
2026-04-29 22:56:41 +0300

    new commit

--- oop/MiniProject.php
+++ oop/MiniProject.php
@@ -12,37 +12,7 @@ public function __construct($name){
 }
 
 class RegularMember  extends BaseMember {
- public function __construct($name)
- {
-    return parent::__construct($name);
- }
-  public function getAccess(){
-    return "you only have access to the gym " .$this->name;
-  }
+ 
 }
-
-class VipMember extends BaseMember {
-    private $localNumber;
-    public function __Construct($name, $localNumber){
-        parent::__construct($name);
-        $this->localNumber = $localNumber;
-    }
-    public function getAccess(){
-        return "You are allowed to enter the lounge, sauna, and swimming pool";
-    }
-    public function getLocker(){
-        return $this->localNumber;
-    }
-
-}
-
-$m1 = new RegularMember("yusf");
-$v1 = new VipMember("ahmed", 20);
-
- echo $m1->getAccess()."<br>";
- echo $v1->getAccess()."<br>";
-
- echo $v1->getLocker();
-
 
 ?>
