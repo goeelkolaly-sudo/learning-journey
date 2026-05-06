8fc6179a00b6989262a3bf9f2f7e99b6351bd26d
yousef <goeelkolaly@gmail.com>
2026-05-06 19:01:20 +0300

    new commit

---
+++ laravel_school2/app/Http/Controllers/StudentsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    public function index(){
+          return 'index';
+    }
+
+     public function show($id){
+        
+    }
+
+     public function create(){
+        
+    }
+
+     public function store(){
+        
+    }
+
+     public function edit(){
+        
+    }
+
+     public function delete(){
+        
+    }
+}
